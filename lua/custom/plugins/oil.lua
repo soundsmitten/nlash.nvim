bbb4ce8abf27d70ff449d94d70a035c715ee51d2
Nick Lash <njlash@fastmail.fm>
2024-04-20 23:42:37 -0500

    pin oil, add keymap

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,6 +1,18 @@
 return {
   'stevearc/oil.nvim',
   opts = {},
+  tag = 'v2.8.0',
+
+  config = function()
+    local oil = require 'oil'
+    oil.setup {
+      -- Set the default keymap to <leader>o
+      vim.keymap.set('n', '<leader>db', function()
+        oil.open(nil)
+      end),
+    }
+  end,
+
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
