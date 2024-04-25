f00d4052ff0494b842fa95c672bcf4540bbbc9cb
Nick Lash <nlash@livly.io>
2024-04-24 20:01:11 -0500

    update lint/conform.

--- init.lua
+++ init.lua
@@ -642,50 +642,82 @@ require('lazy').setup({
       }
     end,
   },
-
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    lazy = false,
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+    local conform = require("conform")
+    
+    conform.setup({
       formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-        --      swift = { 'swiftformat' },
+        swift = { "swiftformat", },
       },
-      --    formatters = {
-      --      swiftformat = {
-      --        command = 'swiftformat',
-      --        args = { 'swiftformat --disable wrapMultilineStatementBraces stripunusedargs hoistPatternLet' },
-      --      },
-      --    },
-    },
+      format_on_save = false,
+      --      format_on_save = function()
+      --        return { timeout_ms = 500, lsp_fallback = true }
+      --      end,
+      log_level = vim.log.levels.ERROR,
+      
+      formatters = {
+        swiftformat = {
+          command = "swiftformat",
+          args = { "--config", "~/.config/nvim/.swiftformat", "--stdinpath", "$FILENAME" },
+          stdin = true,
+          condition = function(ctx)
+          return vim.fs.basename(ctx.filename) ~= "README.md"
+          end
+        },
+      },
+      vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+        end, { desc = "Format file or range (in visual mode)" })
+    })
+    end,
   },
+--{ -- Autoformat
+--  'stevearc/conform.nvim',
+--  lazy = false,
+--  keys = {
+--    {
+--      '<leader>f',
+--      function()
+--        require('conform').format { async = true, lsp_fallback = true }
+--      end,
+--      mode = '',
+--      desc = '[F]ormat buffer',
+--    },
+--  },
+--  
+--  
+--  opts = {
+--    notify_on_error = false,
+--    format_on_save = function(bufnr)
+--      -- Disable "format_on_save lsp_fallback" for languages that don't
+--      -- have a well standardized coding style. You can add additional
+--      -- languages here or re-enable it for the disabled ones.
+--      local disable_filetypes = { c = true, cpp = true }
+--      return {
+--        timeout_ms = 500,
+--        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+--      }
+--    end,
+--    formatters_by_ft = {
+--      
+----      swift = { "swiftformat_ext" },
+--    },
+--    -- format_on_save = {
+--    -- 	timeout_ms = 500,
+--    -- 	lsp_fallback = true,
+--    -- },
+--    format_on_save = true,
+--    log_level = vim.log.levels.ERROR,
+--          },
+--  },
+--},
 
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -957,3 +989,4 @@ opt.swapfile = false
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+  
--- lua/custom/plugins/lint.lua
+++ lua/custom/plugins/lint.lua
@@ -44,20 +44,20 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
---    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
---    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
---      group = lint_augroup,
---      callback = function()
---        require('lint').try_lint()
---      end,
---    })
---
---    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave', 'TextChanged' }, {
---      group = lint_augroup,
---      callback = function()
---        require('lint').try_lint()
---      end,
---    })
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+
+      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave', 'TextChanged' }, {
+        group = lint_augroup,
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
 
       vim.keymap.set('n', '<leader>ml', function()
         require('lint').try_lint()
