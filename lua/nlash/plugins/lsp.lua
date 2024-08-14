63b7b34396948f22e5ccc287e6fea0961c2d57ae
Nick Lash <njlash@fastmail.fm>
2024-08-13 22:21:11 -0500

    use gopls for golang

--- lua/nlash/plugins/lsp.lua
+++ lua/nlash/plugins/lsp.lua
@@ -203,6 +203,23 @@ return { -- LSP Configuration & Plugins
       on_attach = on_attach,
     }
 
+    lspconfig.gopls.setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      cmd = { 'gopls' },
+      filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+      root_dir = lspconfig.util.root_pattern('go.work', 'go.mod', '.git'),
+      settings = {
+        gopls = {
+          completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+          },
+        },
+      },
+    }
+
     -- nice icons
     local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
     for type, icon in pairs(signs) do
