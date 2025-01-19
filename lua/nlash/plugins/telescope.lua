20dac995877d71cb18f4794c039d3d051be9470c
Nick Lash <njlash@fastmail.fm>
2025-01-19 13:46:39 -0600

    remap commandline : to telescope cmd

--- lua/nlash/plugins/telescope.lua
+++ lua/nlash/plugins/telescope.lua
@@ -100,13 +100,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
     vim.keymap.set('n', '<leader>sm', extensions.media_files.media_files, { desc = '[S]earch [M]edia Files' })
 
-    -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown())
     end, { desc = '[/] Fuzzily search in current buffer' })
 
     -- It's also possible to pass additional configuration options.
