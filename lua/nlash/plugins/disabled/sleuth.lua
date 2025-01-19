5f3dfd10c421acef9dfb064b6513191cc0ff13cd
Nick Lash <njlash@fastmail.fm>
2025-01-19 14:13:31 -0600

    disable more plugins

---
+++ lua/nlash/plugins/disabled/sleuth.lua
@@ -0,0 +1,21 @@
+# $HOME/.config/alacritty/alacritty.toml
+# by Rito Ghosh 2023-10-06
+
+[window]
+
+opacity = 0.9
+
+padding.x = 10
+padding.y = 10
+
+decorations = "Full"
+decorations_theme_variant = "Light" # "Dark"
+
+[font]
+
+normal.family = "CaskaydiaCove Nerd Font"
+bold.family = "CaskaydiaCove Nerd Font"
+italic.family = "CaskaydiaCove Nerd Font" 
+bold_italic.family = "CaskaydiaCove Nerd Font"
+
+size = 18.0
