f710c2a7e868c8d5d9461811ebc7b8fb1dae7a33
Nick Lash <njlash@fastmail.fm>
2025-01-21 00:02:17 -0600

    use mini pairs.

--- lua/nlash/plugins/mini.lua
+++ lua/nlash/plugins/mini.lua
@@ -1,6 +1,7 @@
 -- Collection of various small independent plugins/modules
 return {
   'echasnovski/mini.nvim',
+  event = 'VeryLazy',
   config = function()
     -- Better Around/Inside textobjects
     --
@@ -9,6 +10,19 @@ return {
     --  - yinq - [Y]ank [I]nside [N]ext [']quote
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
+
+    require('mini.pairs').setup {
+       modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
