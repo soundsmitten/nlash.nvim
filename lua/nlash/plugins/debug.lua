766fc3277ebc6fb49d8c5669a75ff450f1fc6e78
Nick Lash <njlash@fastmail.fm>
2025-02-04 19:45:55 -0600

    get rid of mason dap (it was conflicting with Xcode lldb)

--- lua/nlash/plugins/debug.lua
+++ lua/nlash/plugins/debug.lua
@@ -97,30 +97,30 @@ return {
     local xcodebuild = require 'xcodebuild.integrations.dap'
 
     -- TODO: change it to your local codelldb path
-    local codelldbPath = os.getenv 'HOME' .. '/.local/bin/codelldb-aarch64-darwin/extension/adapter/codelldb'
+    local codelldbPath = os.getenv 'HOME' .. '/.local/bin/codelldb-darwin-arm64/extension/adapter/codelldb'
 
     xcodebuild.setup(codelldbPath)
     setupListeners()
     local dap = require 'dap'
     local dapui = require 'dapui'
-
-    require('mason-nvim-dap').setup {
-      automatic_installation = true,
-
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_setup = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-      },
-    }
+    --
+    -- require('mason-nvim-dap').setup {
+    --   automatic_installation = true,
+    --
+    --   -- Makes a best effort to setup the various debuggers with
+    --   -- reasonable debug configurations
+    --   automatic_setup = true,
+    --
+    --   -- You can provide additional configuration to the handlers,
+    --   -- see mason-nvim-dap README for more information
+    --   handlers = {},
+    --
+    --   -- You'll need to check that you have the required things installed
+    --   -- online, please don't ask me how to install them :)
+    --   ensure_installed = {
+    --     -- Update this to ensure that you have the debuggers for the langs you want
+    --   },
+    -- }
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
