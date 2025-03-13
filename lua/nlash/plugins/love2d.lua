d9004df5f97a41dbad996151e58ae817cdea974a
Nick Lash <njlash@fastmail.fm>
2025-03-12 21:47:48 -0500

    re-enable love2d + add csharp dap

---
+++ lua/nlash/plugins/love2d.lua
@@ -0,0 +1,10 @@
+return {
+  'S1M0N38/love2d.nvim',
+  cmd = 'LoveRun',
+  opts = {},
+  keys = {
+    { '<leader>l', ft = 'lua', desc = 'LÖVE' },
+    { '<leader>lr', '<cmd>LoveRun<cr>', ft = 'lua', desc = 'Run LÖVE' },
+    { '<leader>ls', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
+  },
+}
