3d6abc02c9d16199a3f158e24e75f50e15601dec
Nick Lash <njlash@fastmail.fm>
2025-03-17 20:28:49 -0500

    add dashboard, replace mini.pairs with autopairs. update colorscheme.

--- lua/nlash/plugins/mini.lua
+++ lua/nlash/plugins/mini.lua
@@ -11,18 +11,18 @@ return {
     --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
 
-    require('mini.pairs').setup {
-      modes = { insert = true, command = true, terminal = false },
-      -- skip autopair when next character is one of these
-      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-      -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { 'string' },
-      -- skip autopair when next character is closing pair
-      -- and there are more closing pairs than opening pairs
-      skip_unbalanced = true,
-      -- better deal with markdown code blocks
-      markdown = true,
-    }
+    -- require('mini.pairs').setup {
+    --   modes = { insert = true, command = false, terminal = false },
+    --   -- skip autopair when next character is one of these
+    --   skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    --   -- skip autopair when the cursor is inside these treesitter nodes
+    --   skip_ts = { 'string' },
+    --   -- skip autopair when next character is closing pair
+    --   -- and there are more closing pairs than opening pairs
+    --   skip_unbalanced = true,
+    --   -- better deal with markdown code blocks
+    --   markdown = true,
+    -- }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
     --
