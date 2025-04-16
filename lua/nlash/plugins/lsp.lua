62aa7ef7dadb038094045ebf957cf5c830b7cb95
Nick Lash <njlash@fastmail.fm>
2025-04-15 23:50:57 -0500

    Replace nvim-cmp with blink.cmp

--- lua/nlash/plugins/lsp.lua
+++ lua/nlash/plugins/lsp.lua
@@ -28,7 +28,7 @@ return { -- LSP Configuration & Plugins
         },
       },
     },
-    'cmp-nvim-lsp',
+    'saghen/blink.cmp',
     { 'antosha417/nvim-lsp-file-operations', config = true },
 
     -- `roslyn` is the official C# LSP used in VSCode.
@@ -164,8 +164,9 @@ return { -- LSP Configuration & Plugins
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+    --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
