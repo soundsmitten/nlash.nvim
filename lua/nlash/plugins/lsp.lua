058898bbe00534f9d854024da0c2c7e5d5d58f91
Nick Lash <njlash@fastmail.fm>
2025-11-03 17:26:31 -0600

    Revert "use telescope for lsp hopping."
    This reverts commit 61ed075136a7284cf6783ada3b106c4f0cf07f2d.

--- lua/nlash/plugins/lsp.lua
+++ lua/nlash/plugins/lsp.lua
@@ -92,19 +92,19 @@ return { -- LSP Configuration & Plugins
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
         -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('gy', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
--- lua/nlash/plugins/snacks.lua
+++ lua/nlash/plugins/snacks.lua
@@ -91,11 +91,6 @@ return {
       cmd = 'lazygit',
       args = {},
     },
-    zen = {
-      toggles = {
-        dim = false,
-      },
-    },
   },
 
   keys = {
@@ -133,62 +128,41 @@ return {
       end,
       desc = 'Lazygit',
     },
-    -- {
-    --   'gd',
-    --   function()
-    --     Snacks.picker.lsp_definitions()
-    --   end,
-    --   desc = 'Goto Definition',
-    -- },
-    -- {
-    --   'gD',
-    --   function()
-    --     Snacks.picker.lsp_declarations()
-    --   end,
-    --   desc = 'Goto Declaration',
-    -- },
-    -- {
-    --   'gr',
-    --   function()
-    --     Snacks.picker.lsp_references()
-    --   end,
-    --   nowait = true,
-    --   desc = 'References',
-    -- },
-    -- {
-    --   'gI',
-    --   function()
-    --     Snacks.picker.lsp_implementations()
-    --   end,
-    --   desc = 'Goto Implementation',
-    -- },
-    -- {
-    --   'gy',
-    --   function()
-    --     Snacks.picker.lsp_type_definitions()
-    --   end,
-    --   desc = 'Goto T[y]pe Definition',
-    -- },
-    -- {
-    --   '<leader>ds',
-    --   function()
-    --     Snacks.picker.lsp_type_definitions()
-    --   end,
-    --   desc = '[D]ocument [S]ymbols',
-    -- },
-    -- {
-    --   '<leader>ws',
-    --   function()
-    --     Snacks.picker.lsp_workspace_symbols()
-    --   end,
-    --   desc = '[W]orkspace [S]ymbols',
-    -- },
     {
-      '<leader>z',
+      'gd',
       function()
-        Snacks.toggle.zen():toggle()
+        Snacks.picker.lsp_definitions()
       end,
-      desc = 'Toggle Zen Mode',
+      desc = 'Goto Definition',
+    },
+    {
+      'gD',
+      function()
+        Snacks.picker.lsp_declarations()
+      end,
+      desc = 'Goto Declaration',
+    },
+    {
+      'gr',
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = 'References',
+    },
+    {
+      'gI',
+      function()
+        Snacks.picker.lsp_implementations()
+      end,
+      desc = 'Goto Implementation',
+    },
+    {
+      'gy',
+      function()
+        Snacks.picker.lsp_type_definitions()
+      end,
+      desc = 'Goto T[y]pe Definition',
     },
   },
 }
