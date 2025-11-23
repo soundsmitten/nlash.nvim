23a66b2022e30ae041ef5aea770e16e3d976ff13
Nick Lash <njlash@fastmail.fm>
2025-11-22 19:22:02 -0600

    cleanup gitsigns.

--- lua/nlash/plugins/gitsigns.lua
+++ lua/nlash/plugins/gitsigns.lua
@@ -28,14 +28,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 
         -- Navigation
 
-        map('n', '[H', function()
-          if vim.wo.diff then
-            vim.cmd.normal { '[c', bang = true }
-          else
-            gitsigns.nav_hunk 'prev'
-          end
-        end, { desc = 'Navigate to first hunk' })
-
         map('n', '[h', function()
           if vim.wo.diff then
             vim.cmd.normal { '[c', bang = true }
@@ -52,44 +44,44 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
           end
         end, { desc = 'Navigate to next hunk' })
 
-        map('n', ']H', function()
-          if vim.wo.diff then
-            vim.cmd.normal { ']c', bang = true }
-          else
-            gitsigns.nav_hunk 'next'
-          end
-        end, { desc = 'Navigate to last hunk' })
-
         -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+        map('n', '<leader>hr', function()
+          if vim.fn.confirm('Reset hunk?', '&Yes\n&No', 2) == 1 then
+            gitsigns.reset_hunk()
+          end
+        end, { desc = 'Reset hunk (with confirmation)' })
+        map('n', '<leader>hR', function()
+          if vim.fn.confirm('Reset entire buffer?', '&Yes\n&No', 2) == 1 then
+            gitsigns.reset_buffer()
+          end
+        end, { desc = 'Reset buffer (with confirmation)' })
 
-        map('n', 'gh', gitsigns.stage_hunk, { desc = 'Stage hunk' })
-        map('n', 'gH', gitsigns.reset_hunk, { desc = 'Reset hunk' })
-
-        map('v', 'gh', function()
+        map('v', '<leader>hs', function()
           gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = 'Stage hunk' })
 
-        map('v', 'gH', function()
-          gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'Reset hunk' })
+        map('v', '<leader>hr', function()
+          if vim.fn.confirm('Reset selected hunk?', '&Yes\n&No', 2) == 1 then
+            gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end
+        end, { desc = 'Reset hunk (with confirmation)' })
 
-        -- map('n', '<leader>gb', gitsigns.stage_buffer)
-        -- map('n', '<leader>gB', gitsigns.reset_buffer)
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
         map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
-
-        map('n', '<leader>hQ', function()
-          gitsigns.setqflist 'all'
-        end, { desc = 'Quickfix list all hunks' })
-
-        map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Quickfix list hunk' })
-
-        -- Toggles
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>tw', gitsigns.toggle_word_diff)
-
+        -- map('n', '<leader>hQ', function()
+        --   gitsigns.setqflist 'all'
+        -- end, { desc = 'Quickfix list all hunks' })
+        --
+        -- map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Quickfix list hunk' })
+        --
+        -- -- Toggles
+        -- map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+        -- map('n', '<leader>tw', gitsigns.toggle_word_diff)
+        --
         -- Text object
-        map({ 'o', 'x' }, 'gh', ':<C-U>Gitsigns select_hunk<CR>')
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
       end,
     }
   end,
