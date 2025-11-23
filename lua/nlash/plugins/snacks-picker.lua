394816f77f0adcabb61b17473aebddc0c2640a26
Nick Lash <njlash@fastmail.fm>
2025-11-22 18:11:19 -0600

    remove lazygit. add picker git helpers

--- lua/nlash/plugins/snacks-picker.lua
+++ lua/nlash/plugins/snacks-picker.lua
@@ -18,13 +18,6 @@ return {
       desc = 'Help',
     },
     {
-      '<leader>sd',
-      function()
-        Snacks.picker.diagnostics()
-      end,
-      desc = 'Help',
-    },
-    {
       '<leader>sk',
       function()
         Snacks.picker.keymaps()
@@ -99,6 +92,7 @@ return {
       desc = 'Find Config File',
     },
 
+    -- lsp
     {
       'gd',
       function()
@@ -155,6 +149,63 @@ return {
         Snacks.picker.lsp_workspace_symbols()
       end,
       desc = 'LSP Workspace Symbols',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = 'Help',
+    },
+    -- git
+    {
+      '<leader>gb',
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = 'Git Branches',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = 'Git Log',
+    },
+    {
+      '<leader>gL',
+      function()
+        Snacks.picker.git_log_line()
+      end,
+      desc = 'Git Log Line',
+    },
+    {
+      '<leader>gs',
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = 'Git Status',
+    },
+    {
+      '<leader>gS',
+      function()
+        Snacks.picker.git_stash()
+      end,
+      desc = 'Git Stash',
+    },
+    {
+      '<leader>gd',
+      function()
+        Snacks.picker.git_diff()
+      end,
+      desc = 'Git Diff (Hunks)',
+    },
+    {
+      '<leader>gf',
+      function()
+        Snacks.picker.git_log_file()
+      end,
+      desc = 'Git Log File',
     },
   },
 }
