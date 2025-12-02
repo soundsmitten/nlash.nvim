4ac48f5bf3122b0ccfd73a3290334b469050d1d4
Nick Lash <njlash@fastmail.fm>
2025-12-01 19:44:44 -0600

    add init.lua auto/user cmds * Add autocommands to toggle relative numbering when moving between modes/focus. * Add usercommand clear arglist and quit user command.

--- lua/nlash/init.lua
+++ lua/nlash/init.lua
@@ -38,3 +38,29 @@ vim.api.nvim_create_user_command('BufOnly', function()
 end, {
   desc = 'Close all buffers except the current one (silent)',
 })
+
+vim.api.nvim_create_user_command('ArgdQ', function()
+  vim.cmd('argd * | quit')
+end, {
+  desc = 'Clear arglist and quit',
+})
+
+local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', {})
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter', 'CmdlineLeave' }, {
+  group = numbertoggle,
+  callback = function()
+    if vim.opt.number and vim.api.nvim_get_mode() ~= 'i' and vim.bo.buftype == '' then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave', 'CmdlineEnter' }, {
+  group = numbertoggle,
+  callback = function()
+    if vim.opt.number and vim.bo.buftype == '' then
+      vim.opt.relativenumber = false
+      vim.cmd 'redraw'
+    end
+  end,
+})
