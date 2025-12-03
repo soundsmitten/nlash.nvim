0ce89d86385a9adb01bd50a1afcf38036599146e
Nick Lash <njlash@fastmail.fm>
2025-12-02 18:47:30 -0600

    remove kickstarter dap comment.

--- lua/nlash/plugins/debug.lua
+++ lua/nlash/plugins/debug.lua
@@ -1,11 +1,4 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
---
+-- Tightly coupled with xcodebuild.nvim plugin
 
 function SetupXcodebuildRosettaBuildArgs()
   local config = require 'xcodebuild.core.config'
