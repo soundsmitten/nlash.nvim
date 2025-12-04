d28cace18843d263d8c4245b80acf6b87ff3ad1a
Nick Lash <njlash@fastmail.fm>
2025-12-03 18:38:06 -0600

    fix autocommand timing issues.

--- lua/nlash/init.lua
+++ lua/nlash/init.lua
@@ -49,9 +49,8 @@ local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', {})
 vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter', 'CmdlineLeave' }, {
   group = numbertoggle,
   callback = function()
-    if vim.opt.number and vim.api.nvim_get_mode() ~= 'i' and vim.bo.buftype == '' then
+    if vim.opt.number:get() and vim.api.nvim_get_mode().mode ~= 'i' and vim.bo.buftype == '' then
       vim.opt.relativenumber = true
-      vim.opt.number = true
     end
   end,
 })
@@ -59,10 +58,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnte
 vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave', 'CmdlineEnter' }, {
   group = numbertoggle,
   callback = function()
-    if vim.opt.number and vim.bo.buftype == '' then
+    if vim.opt.number:get() and vim.bo.buftype == '' then
       vim.opt.relativenumber = false
-      vim.opt.number = true
-      vim.cmd 'redraw'
     end
   end,
 })
