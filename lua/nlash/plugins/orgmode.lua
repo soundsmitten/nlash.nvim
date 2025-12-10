1c1d14c7072e01e045c7353cba9dca5e02a31258
Nick Lash <njlash@fastmail.fm>
2025-12-09 18:28:04 -0600

    replace blink-indent with snacks-indent. disable auto-fold in orgmode

--- lua/nlash/plugins/orgmode.lua
+++ lua/nlash/plugins/orgmode.lua
@@ -8,5 +8,16 @@ return {
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/inbox.org',
     }
+
+    -- Prevent folds from collapsing when exiting insert mode
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'org',
+      callback = function()
+        vim.opt_local.foldmethod = 'expr'
+        vim.opt_local.foldexpr = 'nvim_treesitter#foldexpr()'
+        vim.opt_local.foldlevel = 99
+        vim.opt_local.foldlevelstart = 99
+      end,
+    })
   end,
 }
---
+++ lua/nlash/plugins/snacks-indent.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    indent = {
+      animate = {
+        enabled = false,
+      }
+      -- your indent configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
